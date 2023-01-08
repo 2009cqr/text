411dd92b603df2c5489bd6e464fc723568f27a7a
2009cqr <122187828+2009cqr@users.noreply.github.com>
2023-01-08 17:47:17 +0800

    Add files via upload

---
+++ 未命名1.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define LF putchar('\n')
+#define SP putchar(' ')
+#define rep(i,a,b) for(register int i=(a);i<=(b);++i)
+#define repp(i,a,b) for(register int i=(a);i>=(b);i--)
+#define pair<int,int> pill
+#define mem(t, v) memset((t),v,sizeof(t))
+#define fir first
+#define sec second
+#define mk make_pair
+#define INF 0x3f3f3f3f
+#define LINF 0x3f3f3f3f3f3f3f3f
+typedef long long ll;
+typedef unsigned long long ull;
+template<typename T>
+void read(T &x) {x=0;char ch=getchar();ll f=1;while(!isdigit(ch)){if(ch=='-')f*=-1;ch=getchar();}while(isdigit(ch)){x=(x<<3)+(x<<1)+(ch^48);ch=getchar();}x*=f;}
+template<typename T, typename... Args>
+void read(T &first, Args& ... args) {read(first);read(args...);}
+template<typename T>
+void write(T arg) {T x=arg;if(x<0) {putchar('-');x=-x;}if(x>9) {write(x/10);}putchar(x%10+'0');}
+template<typename T, typename ... Ts>
+void write(T arg, Ts ... args) {write(arg);if(sizeof...(args)!=0) {putchar(' ');write(args ...);}}
+const ll MOD=1e9+7,N=1e6+10;
+int n,m,f[N],dis[N],num[N],x,y; 
+char op;
+int Find(int x){
+	if(f[x]==x)return x;
+	int root=Find(f[x]);
+	dis[x]+=dis[f[x]];
+	return f[x]=root;
+}
+void U(int x,int y){
+	int xx=Find(x),yy=Find(y);
+	if(xx!=yy){
+		f[xx]=f[yy];
+		dis[xx]=num[yy];
+		num[yy]+=num[xx];
+	}
+}
+signed main(){
+//	freopen(".in","r",stdin);
+//	freopen(".out","w",stdout);
+	cin>>n>>m;
+	rep(i,1,n){
+		f[i]=i;
+		num[i]=1;
+	}
+	while(m--){
+		cin>>op;
+		if(op=='C'){
+			cin>>x;
+			Find(x);
+			write(dis[x]);
+			LF;
+		}
+		else{
+			cin>>x>>y;
+			U(x,y);
+		}
+	}
+//	fclose(stdin);
+//	fclose(stdout);
+	return 0;
+}
+
